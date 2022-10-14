1557db74bf646bef1e1702f2c1f85085a69a33b2
Quangdai2411 <93960692+Quangdai2411@users.noreply.github.com>
2022-10-14 10:32:11 +0700

    code

---
+++ JS/Lesson5/js/inner-text-and-text-content.js
@@ -0,0 +1,11 @@
+var firstItem = document.getElementById('one');
+var showTextContent = firstItem.textContent;
+var showInnerText = firstItem.innerText;
+
+var msg = '<p>textContent : ' + showTextContent + '</p>';
+    msg += '<p>inerText : ' + showInnerText + '</p>';
+var el = document.getElementById('scriptResults');
+el.innerHTML = msg;
+
+firstItem.textContent = 'sourdough bread';
+
---
+++ JS/Lesson5/js/set-attribute.js
@@ -0,0 +1,5 @@
+var firstItem = document.getElementById('one');
+firstItem.className = 'complete';
+
+var fourthIteam = document.getElementsByTagName('li').item(3);
+fourthIteam.setAttribute('class', 'cool');
