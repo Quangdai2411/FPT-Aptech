e56be9491b3a976b97532f2a83e01eb2e8ed9650
Quangdai2411 <93960692+Quangdai2411@users.noreply.github.com>
2023-05-15 11:38:02 +0700

    CODE

---
+++ C#/Atomic.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+
+class Atomic
+{
+    public int number;
+    public string symbol;
+    public string name;
+    public double weight;
+
+
+    public Atomic(int number, string symbol, string name, double weight)
+    {
+        this.number = number;
+        this.symbol = symbol;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public Atomic()
+    {
+    }
+
+    ArrayList arr = new ArrayList();
+
+    public Boolean accept()
+    {
+        System.Console.Write("Enter atomic number : ");
+        number = Convert.ToInt32(System.Console.ReadLine());
+        System.Console.Write("Enter symbol : ");
+        symbol = Console.ReadLine();
+        System.Console.Write("Enter full name : ");
+        name = Console.ReadLine();
+        System.Console.Write("Enter atomic weight : ");
+        weight = Convert.ToDouble(System.Console.ReadLine()); 
+
+        Atomic atm = new Atomic(number, symbol, name, weight);
+        arr.Add(atm);
+        return true;
+    }
+
+    public void display()
+    {
+        foreach(Atomic at in arr)
+        {
+            System.Console.WriteLine(at.number + " " + at.symbol + " " + at.name + at.weight);
+        }
+    }
+}
+
---
+++ C#/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    public static void Main(String[] args)
+    {
+        Atomic at = new Atomic();
+        int i = 0;
+        System.Console.WriteLine("Atomic Information");
+        System.Console.WriteLine("==================");
+        do
+        {
+            i++;
+            at.accept();
+        } while (i != 3);
+        System.Console.WriteLine("No Sym Name Weight");
+        System.Console.WriteLine("-----------------------------");
+        at.display();
+
+
+    } 
+}
