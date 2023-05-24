d2cacdb5dfb77b22d049cb18be4d2da2391e163d
Quangdai2411 <93960692+Quangdai2411@users.noreply.github.com>
2023-05-24 09:10:52 +0700

    code

---
+++ C#/Circle/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        Cirle c = new Cirle(2.45, "red", 23);
+        Console.WriteLine("Circle before change : " + c.ToString());
+
+        c.PColor = "blue";
+        c.PWeight = 2.56;
+
+        Console.WriteLine("Circle after change : " + c.ToString());
+
+        Console.WriteLine("------------------");
+        Console.WriteLine(c.findArea());
+        Console.WriteLine(c.findPerimeter());
+
+        Console.ReadLine();
+    }
+}
