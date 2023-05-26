d1d01051334950d0efee2d8103a45bc23183a49b
Quangdai2411 <93960692+Quangdai2411@users.noreply.github.com>
2023-05-26 15:58:53 +0700

    code

---
+++ C#/Bai 1/Program.cs
@@ -0,0 +1,122 @@
+﻿namespace Bai1
+{
+    class LibraryItem
+    {
+        protected string Title;
+        protected string Author;
+        protected int PublicationYear;
+
+        public LibraryItem(string title, string author, int publicationYear)
+        {
+            Title = title;
+            Author = author;
+            PublicationYear = publicationYear;
+        }
+
+        public LibraryItem()
+        {
+        }
+
+        public void Input()
+        {
+            Console.Write("Enter title: ");
+            Title = Console.ReadLine();
+            Console.Write("Enter Author: ");
+            Author = Console.ReadLine();
+            Console.Write("Enter PublicationYear: ");
+            PublicationYear = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public virtual String ToString()
+        {
+            return "Title is: " + Title + "\nAuthor is: " + Author + "\nPublicationYear is: " + PublicationYear;
+        }
+    }
+
+    class Book : LibraryItem
+    {
+        protected int PageNumbers;
+
+        public Book()
+        {
+        }
+        public void InputPage()
+        {
+            Console.Write("Enter PageNumber : ");
+            PageNumbers = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        public override String ToString()
+        {
+            return "Title is: " + Title + "\nAuthor is: " + Author + "\nPublicationYear is: " + PublicationYear+ "\nPageNumbers is: "+PageNumbers;
+        }
+    }
+
+    class Magazine : LibraryItem
+    {
+        protected string Id;
+
+        public Magazine()
+        {
+        }
+
+        public void InputId()
+        {
+            Console.WriteLine("Enter ID : ");
+            Id = Console.ReadLine();
+        }
+
+        public override String ToString()
+        {
+            return "Title is: " + Title + "\nAuthor is: " + Author + "\nPublicationYear is: " + PublicationYear + "\nId is: " + Id;
+
+        }
+    }
+    
+    class DVD : LibraryItem
+    {
+        protected string Category;
+
+        public DVD()
+        {
+        }
+
+        public void InputCatogery()
+        {
+            Console.WriteLine("Enter Category : ");
+            Category = Console.ReadLine();
+        }
+
+        public override string ToString()
+        {
+            return "Title is: " + Title + "\nAuthor is: " + Author + "\nPublicationYear is: " + PublicationYear + "\nCategory is: " + Category;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b = new Book();
+            Magazine m = new Magazine();
+            DVD d = new DVD();
+            b.Input();
+            b.InputPage();
+            Console.WriteLine("-----------------");
+            Console.WriteLine(b.ToString());
+            Console.WriteLine("===");
+
+            m.Input();
+            m.InputId();
+            Console.WriteLine("-----------------");
+            Console.WriteLine(m.ToString());
+            Console.WriteLine("===");
+
+            d.Input();
+            d.InputCatogery();
+            Console.WriteLine("-----------------");
+            Console.WriteLine(d.ToString());
+        }
+    }
+}
---
+++ C#/Bai 2/Program .cs
@@ -0,0 +1,126 @@
+﻿namespace bt2
+{
+    class GameItem
+    {
+        protected string Name;
+        protected int Level;
+        protected int Score;
+
+
+        public GameItem()
+        {
+        }
+
+        public void Infor()
+        {
+            Console.Write("Enter name: ");
+            Name = Console.ReadLine();
+            Console.Write("Enter level: ");
+            Level = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Score: ");
+            Score = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public virtual String ToString()
+        {
+            return "Name is: " + Name + "\nLever is: " + Level + "\nScore is: " + Score;
+        }
+    }
+
+    class Weapon : GameItem
+    {
+        protected string WeaponName;
+        protected int Dame;
+
+        public Weapon()
+        {
+        }
+        public void InputDame()
+        {
+            Console.Write("Enter Weapon : ");
+            WeaponName = Console.ReadLine();
+            Console.Write("Enter Dame: ");
+            Dame = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        public override String ToString()
+        {
+            return "Name is: " + Name + "\nLever is: " + Level + "\nScore is: " + Score + "\nWeapon : " + WeaponName +"\nDame is: "+Dame;
+        }
+    }
+
+    class Amor : GameItem
+    {
+        protected string AmorName;
+        protected int Hp;
+
+        public Amor()
+        {
+        }
+
+        public void InputAmor()
+        {
+            Console.Write("Enter Amor : ");
+            AmorName = Console.ReadLine();
+            Console.Write("Enter Hp: ");
+            Hp = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public override String ToString()
+        {
+            return "Name is: " + Name + "\nLever is: " + Level + "\nScore is: " + Score+ "\nAmor : "+ AmorName +"\nHp is: "+ Hp;
+
+        }
+    }
+
+    class Consumable : GameItem
+    {
+        protected string NameItem;
+        protected string Action;
+
+        public Consumable()
+        {
+        }
+
+        public void InputItem()
+        {
+            Console.Write("Enter Item : ");
+            NameItem = Console.ReadLine();
+            Console.Write("Enter Action :");
+            Action = Console.ReadLine();
+        }
+
+        public override string ToString()
+        {
+            return "Name is: " + Name + "\nLever is: " + Level + "\nScore is: " + Score+"\nNameItem is: " + NameItem + "\nAction : " + Action;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon w = new Weapon();
+            Amor a = new Amor();
+            Consumable c = new Consumable();
+
+            w.Infor();
+            w.InputDame();
+            Console.WriteLine("-----------");
+            Console.WriteLine(w.ToString());
+
+            Console.WriteLine("===");
+            a.Infor();
+            a.InputAmor();
+            Console.WriteLine("------------");
+            Console.WriteLine(a.ToString());
+
+            Console.WriteLine("===");
+            c.Infor();
+            c.InputItem();
+            Console.WriteLine("------------");
+            Console.WriteLine(c.ToString());
+        }
+    }
+}
