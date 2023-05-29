0f88d1ecb94c82bec4492eba45e735a2009ef911
Quangdai2411 <93960692+Quangdai2411@users.noreply.github.com>
2023-05-29 11:15:34 +0700

    code

---
+++ C#/Hopstital/DoctorTable.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hopstital
+{
+    class DoctorTable
+    {
+        public Hashtable objDoctorDetails = new Hashtable();
+
+        public void AcceptDetails()
+        {
+            string name;
+            string address;
+           
+            char choice = 'Y';
+
+            try
+            {
+                do
+                {                 
+                    Console.Write("Enter the doctor's name : ");
+                    name = Console.ReadLine();
+                    Console.Write("Enter the address : ");
+                    address = Console.ReadLine();
+                    objDoctorDetails.Add(name, address);
+                    Console.Write("Do you want to add more records? [Y/n] : ");
+                    choice = Convert.ToChar(Console.ReadLine());
+
+                } while (choice == 'Y' || choice == 'y');
+            }catch(Exception objEx)
+            {
+                Console.WriteLine("Error : " + objEx.Message);
+            }
+        }
+
+        public void DisplayDetails()
+        {
+            ICollection objCollection = objDoctorDetails.Keys;
+
+            Console.WriteLine("\nDetails of doctor : ");
+            Console.WriteLine("Doctor's name \t Address");
+            Console.WriteLine("------------- \t -------");
+            
+            foreach(string details in objCollection)
+            {
+                Console.WriteLine(details + "\t\t" + objDoctorDetails[details]);
+            }
+
+            Console.WriteLine("Total number of doctors : " + objDoctorDetails.Count);
+        }
+
+        public bool Remove()
+        {
+            Console.Write("Enter the name of the doctor : ");
+            string choice = Console.ReadLine();
+
+            if (objDoctorDetails.ContainsKey(choice))
+            {
+                objDoctorDetails.Remove(choice);
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public void Search()
+        {
+            Console.Write("Enter the name of the doctor : ");
+            string choice = Console.ReadLine();
+
+            if(choice != "")
+            {
+                if(objDoctorDetails.Contains(choice))
+                {
+                    Console.WriteLine("Record Found!");
+                    Console.WriteLine("Doctor's Name : " + choice);
+
+                    Console.WriteLine("Address : " + objDoctorDetails[choice]);
+                }
+                else
+                    Console.WriteLine("Record Not Found!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+        }
+
+        public bool Update()
+        {
+            Console.Write("Nhap bac si muon sua : ");
+            string choice = Console.ReadLine();
+
+            if (objDoctorDetails.ContainsKey(choice))
+            {
+                Console.Write("Sua dia chi thanh : ");
+                string b = Console.ReadLine();
+
+                objDoctorDetails[choice] = b;
+                return true;
+            }else
+                return false;
+            
+        }
+    }
+}
---
+++ C#/Hopstital/DoctorTableTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hopstital
+{
+    class DoctorTableTest
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DoctorTable objDoctorTable = new DoctorTable();
+                objDoctorTable.AcceptDetails();
+                objDoctorTable.DisplayDetails();
+
+                char input = 'Y';
+
+                do
+                {
+                    Console.WriteLine("\nSelect one of the following options : ");
+                    Console.WriteLine("1. Remove \n2. RemoveAll \n3. Search \n4. Update \n5. Exit");
+                    Console.Write("Enter your choice : ");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            if (objDoctorTable.Remove())
+                            {
+                                Console.WriteLine("\n----After Removing----");
+                                objDoctorTable.DisplayDetails();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Doctor with this name does not exist");
+                            }
+                            break;
+                        case 2:
+                            objDoctorTable.objDoctorDetails.Clear();
+                            Console.WriteLine("\n----After removing all the doctors----");
+                            Console.WriteLine("Total number of doctor : " + objDoctorTable.objDoctorDetails.Count);
+                            return;
+                        case 3:
+                            objDoctorTable.Search();
+                            break;
+                        case 4:
+                            objDoctorTable.Update();
+                            objDoctorTable.DisplayDetails();
+                            break;
+                        case 5:
+                            return;
+                        default:
+                            Console.WriteLine("Invalid Data Entry!");
+                            break;
+                    }
+                }while (input == 'Y' || input == 'y');
+            }catch (Exception objEx)
+            {
+                Console.WriteLine("Error ; " + objEx.Message);
+            }
+        }
+    }
+}
