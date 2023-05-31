92788f4a1314d66c213e9b94ab4726bfa47403e8
Quangdai2411 <93960692+Quangdai2411@users.noreply.github.com>
2023-05-31 09:45:49 +0700

    code

---
+++ C#/Animal/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Animal;
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tiger t = new Tiger();
+            Lion l = new Lion();
+
+            t.SetMe("Tiger", 100);
+            t.Show();
+
+            l.SetMe("Lion", 200);
+            l.Show();
+        }
+    }
+}
