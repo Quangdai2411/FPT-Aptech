05a08ea3f9b96deaefd58645d98d00c1741642aa
Quangdai2411 <93960692+Quangdai2411@users.noreply.github.com>
2023-05-31 09:50:29 +0700

    code

---
+++ C#/ThiC#/Ex1/Cylinder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricExample
+{
+    class Cylinder
+    {
+        protected double radius;
+        protected double height;
+        public double BaseArea, LateralArea, TotalArea, Volumn;
+
+        public Cylinder(double radius, double height)
+        {
+            this.radius = radius;
+            this.height = height;
+        }
+
+        public Cylinder() { }
+
+        public void Process()
+        {
+            Console.WriteLine("Enter the dimenstions of the cylinder");
+            Console.Write("Radius : ");
+            radius = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Height : ");
+            height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            BaseArea = radius * radius * Math.PI;
+            LateralArea = 2 * Math.PI * radius * height;
+            TotalArea = 2 * Math.PI * radius * (height + radius);
+            Volumn = Math.PI * radius * radius * height;
+
+            BaseArea = Math.Round(BaseArea, 2);
+            LateralArea = Math.Round(LateralArea, 2);
+            TotalArea = Math.Round(TotalArea, 2);
+            Volumn = Math.Round(Volumn, 2);
+        }
+
+        public void Result()
+        {
+            Console.WriteLine("Cylinder Characteristtics");
+            Console.WriteLine("Radius : {0}, Height : {1}", radius, height);
+            Console.WriteLine("Base : " + BaseArea + " | " + "Lateral : " + LateralArea + " | " + "Total : " + TotalArea + " | " + "Volumn : " + Volumn);
+        }
+    }
+}
---
+++ C#/ThiC#/Ex1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GeometricExample;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cylinder cy = new Cylinder();
+        cy.Process();
+        cy.Result();
+    }
+}
